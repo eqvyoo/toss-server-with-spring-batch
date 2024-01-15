e482ad389b7253fc2a576d17f9ee2b91b334773c
eqvyoo <ys07106@gmail.com>
2024-01-16 00:56:46 +0900

    initial upload

---
+++ src/main/java/com/eqvyoo/my_spring_batch/MySpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.eqvyoo.my_spring_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBatchApplication.class, args);
+    }
+
+}
